812151bd64cd18721093559f90b618de49748a6a
ds283 <D.Seery@sussex.ac.uk>
2022-04-07 09:07:40 +0100

    Reposition particle tables

--- LifetimeKit/lifetimekit.py
+++ LifetimeKit/lifetimekit.py
@@ -62,6 +62,29 @@ Gram = Kilogram / 1000.0
 SolarMass = 1.98847E30 * Kilogram
 
 
+# table of primary Hawking quanta and masses, taken from BlackHawk manual (Appendix C, Table 3)
+# https://blackhawk.hepforge.org/manuals/blackhawk1.1.pdf
+_SM_particle_table = \
+    {'Higgs': {'mass': 1.2503E2, 'dof': 1},
+     'photon': {'mass': 0, 'dof': 2},
+     'gluon': {'mass': 0.2, 'dof': 16},
+     'W boson': {'mass': 8.0403E1, 'dof': 6},
+     'Z boson': {'mass': 9.11876E1, 'dof': 3},
+     'neutrino': {'mass': 0, 'dof': 6},
+     'electron': {'mass': 5.109989461E-4, 'dof': 4},
+     'muon': {'mass': 1.056583745E-1, 'dof': 4},
+     'tau': {'mass': 1.77686, 'dof': 4},
+     'up quark': {'mass': 2.2E-3, 'dof': 12},
+     'down quark': {'mass': 4.7E-3, 'dof': 12},
+     'charm quark': {'mass': 1.27, 'dof': 12},
+     'strange quark': {'mass': 9.6E-2, 'dof': 12},
+     'top quark': {'mass': 1.7321E2, 'dof': 12},
+     'bottom quark': {'mass': 4.18, 'dof': 12}}
+
+_RS_bulk_particle_table = \
+    {'5D graviton': {'mass': 0, 'dof': 5}}
+
+
 # The *RandallSundrumParameters* class captures details of the 4D and 5D Planck masses, and uses these to compute derived
 # quantities such as the brane tension and the crossover temperature (in GeV and Kelvin) from the quadratic
 # Hubble regime to the linear regime. It also computes the AdS radius in inverse GeV and metres, assuming that
@@ -407,29 +430,6 @@ class PBHModel:
             return Const_2Pi
 
         return Const_4Pi
-
-
-# table of primary Hawking quanta and masses, taken from BlackHawk manual (Appendix C, Table 3)
-# https://blackhawk.hepforge.org/manuals/blackhawk1.1.pdf
-_SM_particle_table = \
-    {'Higgs': {'mass': 1.2503E2, 'dof': 1},
-     'photon': {'mass': 0, 'dof': 2},
-     'gluon': {'mass': 0.2, 'dof': 16},
-     'W boson': {'mass': 8.0403E1, 'dof': 6},
-     'Z boson': {'mass': 9.11876E1, 'dof': 3},
-     'neutrino': {'mass': 0, 'dof': 6},
-     'electron': {'mass': 5.109989461E-4, 'dof': 4},
-     'muon': {'mass': 1.056583745E-1, 'dof': 4},
-     'tau': {'mass': 1.77686, 'dof': 4},
-     'up quark': {'mass': 2.2E-3, 'dof': 12},
-     'down quark': {'mass': 4.7E-3, 'dof': 12},
-     'charm quark': {'mass': 1.27, 'dof': 12},
-     'strange quark': {'mass': 9.6E-2, 'dof': 12},
-     'top quark': {'mass': 1.7321E2, 'dof': 12},
-     'bottom quark': {'mass': 4.18, 'dof': 12}}
-
-_RS_bulk_particle_table = \
-    {'5D graviton': {'mass': 0, 'dof': 5}}
 
 
 class BaseLifetimeModel:
