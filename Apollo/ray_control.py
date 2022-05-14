113842b3e250080cfdcb2c60ab1bcb3aed98cb24
ds283 <D.Seery@sussex.ac.uk>
2022-05-15 00:19:29 +0100

    More tweaks to ray_control.py

--- Apollo/ray_control.py
+++ Apollo/ray_control.py
@@ -24,9 +24,9 @@ head_slots = slots.iloc[0]
 
 if args.start:
     print('** Allocated head node "{name}", slots={slots}'.format(name=head_addr, slots=head_slots))
-    head = fabric.Connection(host=head_addr, user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && source activate ray && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray start --head --num-cpus {n}'.format(n=head_slots))
+    head = fabric.Connection(host=head_addr, user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray start --head --num-cpus {n}'.format(n=head_slots))
 elif args.stop:
-    head = fabric.Connection(host=head_addr, user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && source activate ray && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray stop')
+    head = fabric.Connection(host=head_addr, user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray stop')
 
 
 workers = []
@@ -35,6 +35,6 @@ if num_rows > 1:
     for row in range(1, num_rows):
         if args.start:
             print('** Allocated worker node "{name}", slots={slots}'.format(name=hosts.iloc[row], slots=slots.iloc[row]))
-            workers.append(fabric.Connection(host=hosts.iloc[row], user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && source activate ray && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray start --address {head_addr} --num-cpus {n}'.format(head_addr=head_addr, n=slots.iloc[row])))
+            workers.append(fabric.Connection(host=hosts.iloc[row], user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray start --address {head_addr} --num-cpus {n}'.format(head_addr=head_addr, n=slots.iloc[row])))
         elif args.stop:
-            workers.append(fabric.Connection(host=hosts.iloc[row], user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && source activate ray && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray stop'))
+            workers.append(fabric.Connection(host=hosts.iloc[row], user='ds283').run('source /mnt/pact/ds283/anaconda3/etc/profile.d/conda.sh && /mnt/pact/ds283/anaconda3/envs/ray/bin/ray stop'))
