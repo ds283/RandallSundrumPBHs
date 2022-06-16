e14efbc32061c7d9d60108bad91cc322b39b73af
ds283 <D.Seery@sussex.ac.uk>
2022-06-16 01:21:17 +0100

    Construct M and J emission rates from BlackMax and Kerr greybody data

---
+++ LifetimeKit/greybody_tables/BlackMax.py
@@ -0,0 +1,135 @@
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
+
+from ..particle_data import _table_merge, SM_particle_base_table
+
+# greybody factors extracted from the BlackMax greybody database
+astar = np.asarray([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5])
+
+xi_dMdt_spin0 = np.asarray([0.016733779047978143,
+                            0.0168586918073312,
+                            0.017274178095103735,
+                            0.01806339600973751,
+                            0.01981392846868953,
+                            0.022271978743540952,
+                            0.026141972205832933,
+                            0.03157017778111371,
+                            0.03904235560627453,
+                            0.048603329916217325,
+                            0.06026203454156726,
+                            0.07455959398972482,
+                            0.0900823424713518,
+                            0.10529176397792649,
+                            0.12021255675192243,
+                            0.13413238116000648])
+
+xi_dMdt_spin0pt5 = np.asarray([0.014582027776027216,
+                               0.015015116912536744,
+                               0.016334158136048806,
+                               0.01857853603385632,
+                               0.021837790811581017,
+                               0.026310227103836913,
+                               0.03229437416908495,
+                               0.04014778568695689,
+                               0.050309153725315456,
+                               0.06332192611567004,
+                               0.07939667978259118,
+                               0.09767208241278501,
+                               0.11780112821755317,
+                               0.1411502275780105,
+                               0.16209417527810932,
+                               0.18380421650155893])
+
+xi_dMdt_spin1 = np.asarray([0.011486475223775734,
+                            0.012494022921475213,
+                            0.015616415782925329,
+                            0.021154682342241344,
+                            0.02959891475690643,
+                            0.0416639735070752,
+                            0.05812621547232826,
+                            0.0807444727511764,
+                            0.11068249171036813,
+                            0.14934064970808883,
+                            0.19847217791624122,
+                            0.2584775713178245,
+                            0.33049028705893563,
+                            0.4161534431589193,
+                            0.5095149046182521,
+                            0.6166645966621302])
+
+xi_dJdt_spin0 = np.asarray([4.2250377141904996e-22,
+                            0.0029893888632798312,
+                            0.006346155977418833,
+                            0.010452955272960097,
+                            0.015697952816438227,
+                            0.022616985372570498,
+                            0.0321816644059754,
+                            0.04423303791899698,
+                            0.06096950608602681,
+                            0.08163607753108654,
+                            0.1069855874438356,
+                            0.14019446285705825,
+                            0.1767215438349438,
+                            0.21543255193396638,
+                            0.25586031239716955,
+                            0.29584767888091074])
+
+xi_dJdt_spin0pt5 = np.asarray([4.963887253352477e-13,
+                               0.005100810519721404,
+                               0.01050848679362666,
+                               0.0166757184827052,
+                               0.024045535382879218,
+                               0.03334518163020552,
+                               0.04541528167553959,
+                               0.06122665561800322,
+                               0.08199152962185881,
+                               0.10932777947133657,
+                               0.14433044959985905,
+                               0.1850176981735065,
+                               0.23241899399459118,
+                               0.29031718336895046,
+                               0.345749348101726,
+                               0.4070831048455746])
+
+xi_dJdt_spin1 = np.asarray([1.4871197939461763e-16,
+                            0.007375712752751172,
+                            0.01612461091553282,
+                            0.027736994548181352,
+                            0.04383255580093854,
+                            0.06639076949156711,
+                            0.09726370081317302,
+                            0.1412383020518842,
+                            0.20090414817170965,
+                            0.28058474271461187,
+                            0.38465567525978234,
+                            0.5165207324733933,
+                            0.681163766666365,
+                            0.8869814885281444,
+                            1.1205783273694725,
+                            1.4009011326594107])
+
+# build splines for each of these functions
+xi_dMdt_spin0_spline = InterpolatedUnivariateSpline(astar, xi_dMdt_spin0)
+xi_dMdt_spin0pt5_spline = InterpolatedUnivariateSpline(astar, xi_dMdt_spin0pt5)
+xi_dMdt_spin1_spline = InterpolatedUnivariateSpline(astar, xi_dMdt_spin1)
+
+xi_dJdt_spin0_spline = InterpolatedUnivariateSpline(astar, xi_dJdt_spin0)
+xi_dJdt_spin0pt5_spline = InterpolatedUnivariateSpline(astar, xi_dJdt_spin0pt5)
+xi_dJdt_spin1_spline = InterpolatedUnivariateSpline(astar, xi_dJdt_spin1)
+
+BlackMax_greybody_table_5D = _table_merge(SM_particle_base_table,
+ {'Higgs': {'xi_M': xi_dMdt_spin0_spline, 'xi_J': xi_dJdt_spin0_spline, 'xi-per-dof': True},
+  'photon': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'gluon': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'W boson': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'Z boson': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'neutrino': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'electron': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'muon': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'tau': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'up quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'down quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'strange quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'charm quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'bottom quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'top quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True}})
---
+++ LifetimeKit/greybody_tables/Kerr.py
@@ -0,0 +1,167 @@
+from math import pi
+
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
+
+from .Friedlander import xi0_spin0_4D, xi0_spin0pt5_4D, xi0_spin1_4D
+from ..particle_data import _table_merge, SM_particle_base_table
+
+# taken from Table I of Dong, Kinney & Stojkovic, arXiv:1511.05642v3
+astar = np.asarray([0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.96, 0.99, 0.999, 0.99999, 1.0])
+
+# the raw values tabulated by Gong et al. Note these refer to Page f and g factors, and not the xi values that
+# we are using
+f0 = np.asarray([7.429E-5,
+                 7.442E-5,
+                 7.319E-5,
+                 7.265E-5,
+                 7.097E-5,
+                 6.996E-5,
+                 7.008E-5,
+                 7.119E-5,
+                 7.969E-5,
+                 1.024E-4,
+                 1.551E-4,
+                 2.283E-4,
+                 2.625E-4,
+                 2.667E-4,
+                 2.667E-4])
+
+f0pt5 = np.asarray([8.185E-5,
+                    8.343E-5,
+                    8.830E-5,
+                    9.669E-5,
+                    1.089E-4,
+                    1.258E-4,
+                    1.487E-4,
+                    1.804E-4,
+                    2.284E-4,
+                    3.195E-4,
+                    4.567E-5,
+                    6.708E-4,
+                    9.253E-4,
+                    1.074E-3,
+                    1.093E-3])
+
+f1 = np.asarray([3.366E-5,
+                 3.580E-5,
+                 4.265E-5,
+                 5.525E-5,
+                 7.570E-5,
+                 1.080E-4,
+                 1.594E-4,
+                 2.450E-4,
+                 4.014E-4,
+                 7.520E-4,
+                 1.313E-3,
+                 2.151E-3,
+                 3.057E-3,
+                 3.555E-3,
+                 3.616E-3])
+
+g0 = np.asarray([8.867E-5,
+                 9.085E-5,
+                 9.391E-5,
+                 1.024E-4,
+                 1.125E-4,
+                 1.281E-4,
+                 1.507E-4,
+                 1.803E-4,
+                 2.306E-4,
+                 3.166E-4,
+                 4.515E-4,
+                 6.160E-4,
+                 6.905E-4,
+                 6.997E-4,
+                 7.006E-4])
+
+g0pt5 = np.asarray([6.161E-4,
+                    6.174E-4,
+                    6.218E-4,
+                    6.299E-4,
+                    6.430E-4,
+                    6.631E-4,
+                    6.946E-4,
+                    7.457E-4,
+                    8.366E-4,
+                    1.034E-3,
+                    1.343E-3,
+                    1.810E-3,
+                    2.340E-3,
+                    2.641E-3,
+                    2.678E-3])
+
+g1 = np.asarray([4.795E-4,
+                 4.895E-4,
+                 5.207E-4,
+                 5.759E-4,
+                 6.599E-4,
+                 7.845E-4,
+                 9.668E-4,
+                 1.245E-3,
+                 1.706E-3,
+                 2.636E-3,
+                 3.976E-3,
+                 5.829E-3,
+                 7.723E-3,
+                 8.730E-3,
+                 8.851E-3])
+
+# convert Page f factors to xi factors
+# for details, see 15 June 2022 calculation "Relationship between Page f, g factors and Friedlander et al. xi factors"
+def _convert_f_to_xi(astar_sample, f_sample):
+    astar_sq = astar_sample * astar_sample
+    return 2.0 * pi * (2.0 + astar_sq + 2.0*np.sqrt(1.0 - astar_sq)) * f_sample
+
+xi_dMdt_spin0_pre = _convert_f_to_xi(astar, f0)
+xi_dMdt_spin0pt5_pre = _convert_f_to_xi(astar, f0pt5)
+xi_dMdt_spin1_pre = _convert_f_to_xi(astar, f1)
+
+# convert Page g factors to xi factors
+# for details, see same calcuation
+def _convert_g_to_xi(astar_sample, g_sample):
+    astar_sq = astar_sample * astar_sample
+    return 2.0 * pi * (1.0 + np.sqrt(1.0 - astar_sq)) * astar_sample * g_sample
+
+xi_dJdt_spin0_pre = _convert_g_to_xi(astar, g0)
+xi_dJdt_spin0pt5_pre = _convert_g_to_xi(astar, g0pt5)
+xi_dJdt_spin1_pre = _convert_g_to_xi(astar, g1)
+
+# these samples don't run all the way down to a*=0, so if we use them to build an interpolating function we will need
+# to extrapolate at low a*. That's a bit worrying because the extrapolation could be uncontrolled, so instead
+# we anchor the low-a* behaviour by adding a sample point at a*=0 using the Friedlander et al. results
+astar = np.insert(astar, 0, 0.0, axis=0)
+
+xi_dMdt_spin0 = np.insert(xi_dMdt_spin0_pre, 0, xi0_spin0_4D)
+xi_dMdt_spin0pt5 = np.insert(xi_dMdt_spin0pt5_pre, 0, xi0_spin0pt5_4D)
+xi_dMdt_spin1 = np.insert(xi_dMdt_spin1_pre, 0, xi0_spin1_4D)
+
+xi_dJdt_spin0 = np.insert(xi_dJdt_spin0_pre, 0, 0.0)
+xi_dJdt_spin0pt5 = np.insert(xi_dJdt_spin0pt5_pre, 0, 0.0)
+xi_dJdt_spin1 = np.insert(xi_dJdt_spin1_pre, 0, 0.0)
+
+# build splines for each of these functions
+xi_dMdt_spin0_spline = InterpolatedUnivariateSpline(astar, xi_dMdt_spin0)
+xi_dMdt_spin0pt5_spline = InterpolatedUnivariateSpline(astar, xi_dMdt_spin0pt5)
+xi_dMdt_spin1_spline = InterpolatedUnivariateSpline(astar, xi_dMdt_spin1)
+
+xi_dJdt_spin0_spline = InterpolatedUnivariateSpline(astar, xi_dJdt_spin0)
+xi_dJdt_spin0pt5_spline = InterpolatedUnivariateSpline(astar, xi_dJdt_spin0pt5)
+xi_dJdt_spin1_spline = InterpolatedUnivariateSpline(astar, xi_dJdt_spin1)
+
+Kerr_greybody_table_5D = _table_merge(SM_particle_base_table,
+ {'Higgs': {'xi_M': xi_dMdt_spin0_spline, 'xi_J': xi_dJdt_spin0_spline, 'xi-per-dof': True},
+  'photon': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'gluon': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'W boson': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'Z boson': {'xi_M': xi_dMdt_spin1_spline, 'xi_J': xi_dJdt_spin1_spline, 'xi-per-dof': True},
+  'neutrino': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'electron': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'muon': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'tau': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'up quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'down quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'strange quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'charm quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'bottom quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True},
+  'top quark': {'xi_M': xi_dMdt_spin0pt5_spline, 'xi_J': xi_dJdt_spin0pt5_spline, 'xi-per-dof': True}})
