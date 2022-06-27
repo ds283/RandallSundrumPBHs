52e5214d7440952da28fe6a42a15c8541db83fc2
ds283 <D.Seery@sussex.ac.uk>
2022-06-27 10:01:03 +0100

    Minor changes to test_spinning5D.py

--- test_spinning5D.py
+++ test_spinning5D.py
@@ -4,9 +4,9 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 sns.set()
 
-# test configuration that shows large lifetime shifts from SpinGrid
-M5_value = 3.684e14
-T_init = 5.0525e13
+# test configuration for which PBH evaporates in 5D regime, but seems to exhibit residual spin in SpinGrid
+M5_value = 2.4517e10
+T_init = 5.1943e8
 
 # a black hole configuration that evaporates quite early, for testing impact of changing F et al.
 # M5_value = 1.3209e16
@@ -28,31 +28,41 @@ T_init = 5.0525e13
 # M5_value = 1.4652e14
 # T_init = 8.4124e12
 
+accreteF = 0.001
+collapsef = 0.395
+
 params = lkit.RS5D.Parameters(M5=M5_value)
 
 num_samples = 500
 
+J_plot_type = 'J'
+J_max_plot_type = 'J/Jmax'
+
 # baseline Friedlander et al. greybody model with J=0
 Friedlander_JJmax0 = lkit.PBHInstance(params, T_init, models=['GreybodyRS5D'], compute_rates=True,
-                                      num_samples=num_samples)
+                                      num_samples=num_samples, accretion_efficiency_F=accreteF,
+                                      collapse_fraction_f=collapsef)
 
 # spinning greybody model with J=0
 Spinning_JJmax0 = lkit.PBHInstance(params, T_init, models=['SpinningRS5D'], compute_rates=True,
-                                   num_samples=num_samples)
-Spinning_JJmax0.angular_momentum_plot('Spinning_astar0_J.pdf', type='J')
-Spinning_JJmax0.angular_momentum_plot('Spinning_astar0_JJmax.pdf', type='J/Jmax')
+                                   num_samples=num_samples, accretion_efficiency_F=accreteF,
+                                   collapse_fraction_f=collapsef)
+Spinning_JJmax0.angular_momentum_plot('Spinning_astar0_J.pdf', type=J_plot_type)
+Spinning_JJmax0.angular_momentum_plot('Spinning_astar0_JJmax.pdf', type=J_max_plot_type)
 
 # spinning greybody model with J/Jmax=0.3
 Spinning_JJmax0pt3 = lkit.PBHInstance(params, T_init, J_over_Jmax=0.3, models=['SpinningRS5D'], compute_rates=True,
-                                      num_samples=num_samples)
-Spinning_JJmax0pt3.angular_momentum_plot('Spinning_astar0pt3_J.pdf', type='J')
-Spinning_JJmax0pt3.angular_momentum_plot('Spinning_astar0pt3_JJmax.pdf', type='J/Jmax')
+                                      num_samples=num_samples, accretion_efficiency_F=accreteF,
+                                      collapse_fraction_f=collapsef)
+Spinning_JJmax0pt3.angular_momentum_plot('Spinning_astar0pt3_J.pdf', type=J_plot_type)
+Spinning_JJmax0pt3.angular_momentum_plot('Spinning_astar0pt3_JJmax.pdf', type=J_max_plot_type)
 
 # spinning greybody model with J/Jmax=0.7
 Spinning_JJmax0pt7 = lkit.PBHInstance(params, T_init, J_over_Jmax=0.7, models=['SpinningRS5D'], compute_rates=True,
-                                      num_samples=num_samples)
-Spinning_JJmax0pt7.angular_momentum_plot('Spinning_astar0pt7_J.pdf', type='J')
-Spinning_JJmax0pt7.angular_momentum_plot('Spinning_astar0pt7_JJmax.pdf', type='J/Jmax')
+                                      num_samples=num_samples, accretion_efficiency_F=accreteF,
+                                      collapse_fraction_f=collapsef)
+Spinning_JJmax0pt7.angular_momentum_plot('Spinning_astar0pt7_J.pdf', type=J_plot_type)
+Spinning_JJmax0pt7.angular_momentum_plot('Spinning_astar0pt7_JJmax.pdf', type=J_max_plot_type)
 
 lifetimes = {
     'Friedlander': Friedlander_JJmax0.lifetimes['GreybodyRS5D'],
