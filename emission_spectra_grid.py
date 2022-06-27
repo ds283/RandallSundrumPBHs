56efeb1289dcc2f4684be8c62d5ca70918b6f140
ds283 <D.Seery@sussex.ac.uk>
2022-06-27 15:01:50 +0100

    Expand emission_spectra_grid.py to compute a range of mass multiplier factors

--- emission_spectra_grid.py
+++ emission_spectra_grid.py
@@ -42,44 +42,52 @@ def compute_emission_rates(serial_batch):
 
             # compute wanted initial mass in GeV, which should be about 20 times heavier than the minimal mass
             M_input = f * M_Hubble
-            M_target = 20.0 * M_input
-            T_target = engine.find_Tinit_from_Minit(M=M_target, units='GeV')
 
-            model = lkit.RS5D_Friedlander.LifetimeModel(engine, accretion_efficiency_F=F,
-                                                        use_Page_suppression=True, use_effective_radius=True)
-            soln = lkit.PBHLifetimeModel(M_target, T_target, model, num_samples=500, compute_rates=False)
+            target_multipliers = [1.0, 2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0, 18.0, 20.0]
+            for n, multiplier in enumerate(target_multipliers):
+                M_target = multiplier * M_input
+                T_target = engine.find_Tinit_from_Minit(M=M_target, units='GeV')
 
-            PBH = lkit.RS5D.SpinlessBlackHole(params, M=soln.M_final, units='GeV', strict=False)
+                model = lkit.RS5D_Friedlander.LifetimeModel(engine, accretion_efficiency_F=F,
+                                                            use_Page_suppression=True, use_effective_radius=True)
+                soln = lkit.PBHLifetimeModel(M_target, T_target, model, num_samples=500, compute_rates=False)
 
-            data = {'serial': _serial,
-                    'timestamp': datetime.now(),
-                    'M5_GeV': M5,
-                    'Minput_GeV': M_input,
-                    'Mtarget_GeV': M_target,
-                    'Minit_GeV': soln.M_init,
-                    'Minit_Gram': soln.M_init/lkit.Gram,
-                    'Minit_to_Mtarget_ratio': soln.M_init/M_target,
-                    'F': F,
-                    'f': f,
-                    'Tinit_GeV': Tinit,
-                    'Tinit_Kelvin': Tinit/lkit.Kelvin,
-                    'lifetime_GeV': soln.T_lifetime,
-                    'lifetime_Kelvin': soln.T_lifetime/lkit.Kelvin,
-                    'Mfinal_5D_GeV': soln.M_final,
-                    'Mfinal_5D_Gram': soln.M_final/lkit.Gram,
-                    'initially_5D': soln.initially_5D,
-                    'finally_5D':PBH.is_5D,
-                    'evaporated': soln.evaporated,
-                    'T_Hawking_GeV': PBH.T_Hawking,
-                    'T_Hawking_Kelvin': PBH.T_Hawking/lkit.Kelvin,
-                    'quarks': -model._dMdt_quarks(0.0, PBH) / (lkit.Gram/lkit.Year),
-                    'leptons': -model._dMdt_leptons(0.0, PBH) / (lkit.Gram/lkit.Year),
-                    'photons': -model._dMdt_photons(0.0, PBH) / (lkit.Gram/lkit.Year),
-                    'gluons': -model._dMdt_gluons(0.0, PBH) / (lkit.Gram/lkit.Year),
-                    'EW bosons': -model._dMdt_EW_bosons(0.0, PBH) / (lkit.Gram/lkit.Year),
-                    'gravitons': -model._dMdt_graviton5D(0.0, PBH) / (lkit.Gram/lkit.Year)}
+                PBH = lkit.RS5D.SpinlessBlackHole(params, M=soln.M_final, units='GeV', strict=False)
 
-        batch.append(data)
+                data = {'serial': _serial,
+                        'timestamp': datetime.now(),
+                        'M5_GeV': M5,
+                        'Minput_GeV': M_input,
+                        'multiplier_serial': n,
+                        'multiplier': multiplier,
+                        'Mtarget_GeV': M_target,
+                        'Minit_GeV': soln.M_init,
+                        'Minit_Gram': soln.M_init/lkit.Gram,
+                        'Minit_to_Mtarget_ratio': soln.M_init/M_target,
+                        'F': F,
+                        'f': f,
+                        'Tinit_GeV': Tinit,
+                        'Tinit_Kelvin': Tinit/lkit.Kelvin,
+                        'lifetime_GeV': soln.T_lifetime,
+                        'lifetime_Kelvin': soln.T_lifetime/lkit.Kelvin,
+                        'Mfinal_GeV': soln.M_final,
+                        'Mfinal_Gram': soln.M_final/lkit.Gram,
+                        'Mmax_GeV': soln.M_max,
+                        'Mmax_Gram': soln.M_max/lkit.Gram,
+                        'initially_5D': soln.initially_5D,
+                        'finally_5D':PBH.is_5D,
+                        'evaporated': soln.evaporated,
+                        'T_Hawking_GeV': PBH.T_Hawking,
+                        'T_Hawking_Kelvin': PBH.T_Hawking/lkit.Kelvin,
+                        'quarks': -model._dMdt_quarks(0.0, PBH) / (lkit.Gram/lkit.Year),
+                        'leptons': -model._dMdt_leptons(0.0, PBH) / (lkit.Gram/lkit.Year),
+                        'photons': -model._dMdt_photons(0.0, PBH) / (lkit.Gram/lkit.Year),
+                        'gluons': -model._dMdt_gluons(0.0, PBH) / (lkit.Gram/lkit.Year),
+                        'EW bosons': -model._dMdt_EW_bosons(0.0, PBH) / (lkit.Gram/lkit.Year),
+                        'gravitons': -model._dMdt_graviton5D(0.0, PBH) / (lkit.Gram/lkit.Year)}
+
+                batch.append(data)
+
         times.append(timer.interval)
 
     return batch
